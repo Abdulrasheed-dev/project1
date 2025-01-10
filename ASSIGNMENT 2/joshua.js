e0985fc185aa7c21cb260281596f74656dd7b164
Abdulrasheed-dev <139999590+Abdulrasheed-dev@users.noreply.github.com>
2025-01-10 04:55:47 -0800

    Add files via upload

---
+++ ASSIGNMENT 2/joshua.js
@@ -0,0 +1,21 @@
+let hh ="we are learming javascript"
+// console.log(hh)
+// document.write(hh)
+// alert( hh)
+
+// const qr ="mr kareeem"
+// console.log(qr)
+
+
+// var muminAndI  = "iamExceptional"
+// document.write(muminAndI)
+
+    //string 
+let ol ="sekinah adedfolakan"
+console.log(ol)
+
+
+    //number
+const pi =  3.14285714;
+const num= pi.toFixed(5)
+console.log(num)
